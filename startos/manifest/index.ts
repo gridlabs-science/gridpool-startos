22d26127373f5466fcb833388a4d11436d53b2ec
keegreil <keegreil@gmail.com>
2026-07-28 18:46:12 -0400

    Pin duplicate-safe SV2 fee jobs

--- startos/manifest/index.ts
+++ startos/manifest/index.ts
@@ -21,7 +21,7 @@ export const manifest = setupManifest({
     sv2: {
       source: {
         dockerTag:
-          'ghcr.io/gridlabs-science/gridpool-sv2-pool:sha-7b2311b',
+          'ghcr.io/gridlabs-science/gridpool-sv2-pool:sha-36465d2',
       },
       arch: ['x86_64', 'aarch64'],
     },
