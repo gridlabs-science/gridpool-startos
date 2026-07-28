adca712b4c2c4d8d7e3a8f0e854945bc4391d980
keegreil <keegreil@gmail.com>
2026-07-28 12:18:55 -0400

    Bump StartOS health check revision

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '0.1.0:6',
+  version: '0.1.0:7',
   releaseNotes: {
     en_US:
       'Adds GridPool network, Bitcoin, relay, and pulse telemetry to the StartOS health dashboard.',
